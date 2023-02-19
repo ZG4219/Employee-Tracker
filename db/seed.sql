d0a2f2c12e71d0adaf32d92668303ccc6b2ce833
Dionne Angeles <jr8dionne@yahoo.com>
2023-02-19 15:05:41 -0800

    modified seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,20 +1,30 @@
-INSERT INTO departments (name)
-VALUES ('Sales'), 
-('Marketing'), 
-('Engineering'), 
-('Accountant'),
-('Human Resources');
-
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO department (department_name)
 VALUES
-  ('Salesperson', 50000, 1),
-  ('Marketing Manager', 75000, 2),
-  ('Software Engineer', 80000, 3),
-  ('HR Manager', 90000, 4);
-
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+  ('HR'),
+  ('Tech'),
+  ('Marketing'),
+  ('Finance'),
+  ('Sales'),
+  ('Engineering'),
+  ('Legal');
+  
+INSERT INTO role (title, salary, department_id)
 VALUES
-  ('John', 'Doe', 1, NULL),
-  ('Jane', 'Doe', 2, NULL),
-  ('Bob', 'Builder', 3, 2),
-  ('Samantha', 'Smith', 4, NULL);
+  ('Recruiter ', 20000, 1),
+  ('Marketer', 30000, 3),
+  ('Software Engineer', 40000, 2),
+  ('Attorney', 200000, 7),
+  ('Salesperson', 130000, 5),
+  ('Engineer', 150000, 6),
+  ('Accountant', 160000, 7),
+  ('Sales Lead', 160000, 5),
+  ('CEO', 400000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Ronald', 'Firbank', 1, 1),
+  ('Virginia', 'Woolf', 2, 2),
+  ('Unica', 'Zurn', 3, 1),
+  ('Octavia', 'Butler', 4, 3),
+  ('John', 'Doe', 5, 1),
+  ('Tom', 'Allen', 6, 3);
