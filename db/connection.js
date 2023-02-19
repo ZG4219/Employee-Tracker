df2b9aad91453b3c283469a4bb928b7c3309bae1
Dionne Angeles <jr8dionne@yahoo.com>
2023-02-19 15:03:22 -0800

    added db.sql

--- db/connection.js
+++ db/connection.js
@@ -1,32 +1,15 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'employee_db'
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error(`Error connecting: ${err.stack}`);
-    return;
-  }
-
-  console.log(`Connected to the employee_db database with thread ID ${connection.threadId}`);
-});
-
-// Example query to retrieve all employees from the database
-connection.query(
-  'SELECT * FROM employees',
-  (error, results) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
-
-    console.log(results);
-  }
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      password: '',
+      database: 'employee_db'
+    },
+    console.log('Connected to the employeeTracker database.')
 );
 
-connection.end();
+module.exports = db;
--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,26 @@
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
 
-CREATE DATABASE employee_db;
-
-USE employee_db;
-
-CREATE TABLE departments (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE role (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL(10,2) NOT NULL,
-  department_id INT NOT NULL,
-  FOREIGN KEY (department_id) REFERENCES departments(id)
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
-CREATE TABLE employees (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES roles(id),
-  FOREIGN KEY (manager_id) REFERENCES employees(id)
+  role_id INTEGER,
+  manager_id INTEGER NULL,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) 
 );
-
-
